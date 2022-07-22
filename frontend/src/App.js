69c4efa3afdd4904661ebc6eb9b4dde8b337e6f5
Mikołaj Simon <mikolajsimon.nt@gmail.com>
2022-07-22 20:40:08 +0200

    Change views

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import SignIn from './components/login';
+import SignUp from './components/registration';
+import { Routes, Route} from "react-router-dom";
+import HomePage from './components/homePage';
+import ChangePassword from './components/changePassword';
+import ResetPassword from './components/resetPassword';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+    <Route path="/" element={<HomePage/>} />
+    <Route path="/register" element={<SignUp/>} />
+    <Route path="/login" element={<SignIn/>} />
+    <Route path="/change" element={<ChangePassword/>} />
+    <Route path="/forgot" element={<ResetPassword/>} />
+    </Routes>
   );
 }
 
---
+++ frontend/src/components/changePassword.js
@@ -0,0 +1,144 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Fragment } from 'react';
+import { blueGrey, indigo } from '@mui/material/colors';
+import icon from "../pictures/icon.jpg"
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="/">
+        Gymshare
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      default: blueGrey[800],
+    },
+    primary: {
+      main: indigo[50],
+    },
+    secondary: {
+      main: indigo[50],
+    }
+  },
+}
+);
+
+export default function ChangePassword() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <Fragment>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main', width: 56, height: 56 }} alt="logo" src={icon}>
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Change Password
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+            inputProps={{ style: { color: "white" } }}
+            InputLabelProps={{ style: { color: '#fff' }} }
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+            inputProps={{ style: { color: "white" } }}
+            InputLabelProps={{ style: { color: '#fff' }} }
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Current password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <TextField
+            inputProps={{ style: { color: "white" } }}
+            InputLabelProps={{ style: { color: '#fff' }} }
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="New password"
+              type="password"
+              id="password"
+              autoComplete="new-password"
+            />
+            <TextField
+            inputProps={{ style: { color: "white" } }}
+            InputLabelProps={{ style: { color: '#fff' }} }
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Repeat new password"
+              type="password"
+              id="password"
+              autoComplete="repeat-new-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Change Password
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="/login" variant="body2">
+                  {"Back to login view"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+    </Fragment>
+  );
+}
