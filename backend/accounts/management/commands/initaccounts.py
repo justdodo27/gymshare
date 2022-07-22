c79cf2cd981f5acf8e2b02a323aebb823f0c637d
Dominik Sawicki <dominik.sawicki52@gmail.com>
2022-07-22 15:02:14 +0200

    Add initaccounts command

---
+++ backend/accounts/management/commands/initaccounts.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+from django.conf import settings
+
+class Command(BaseCommand):
+    """
+    Create a list of accounts if none exist
+    Example:
+        manage.py initaccounts
+    """
+
+    def handle(self, *args, **options):
+        if User.objects.count() == 0:
+            for user in settings.ADMINS:
+                username = user[0]
+                email = user[1]
+                password = user[2]
+                print(f"Creating admin account for {username} ({email}) - {password}")
+                admin = User.objects.create_user(username=username, password=password, email=email)
+                admin.is_active = True
+                admin.is_staff = True
+                admin.is_superuser = True
+                admin.save()
+            for user in settings.USERS:
+                username = user[0]
+                email = user[1]
+                password = user[2]
+                print(f"Creating user account for {username} ({email})")
+                account = User.objects.create_user(username=username, password=password, email=email)
+                account.is_active = True
+                account.save()
+        else:
+            print('Accounts already created.')
+
+        
