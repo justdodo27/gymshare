e4c0bd9b5f5848ce6e25489e755b9a03ff068a93
Mikołaj Simon <mikolajsimon.nt@gmail.com>
2022-07-22 20:40:08 +0200

    Change views

---
+++ frontend/src/components/homePage.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Header from "../components/header.js"
+import { blueGrey, indigo } from '@mui/material/colors';
+
+
+const sections = [
+  { title: 'Sign Up', url: '/register' },
+  { title: 'Sign In', url: '/login' },
+  { title: 'Change Password', url: '/change' },
+  { title: 'Forgot Password', url: '/forgot' },
+];
+
+
+const theme = createTheme({
+    palette: {
+      mode: "dark",
+      background: {
+        default: blueGrey[800],
+      },
+      primary: {
+        main: indigo[50],
+      },
+      secondary: {
+        main: indigo[50],
+      }
+    },
+  }
+  );
+
+export default function Blog() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Header title="Gymshare" sections={sections} />
+      </Container>
+    </ThemeProvider>
+  );
+}
