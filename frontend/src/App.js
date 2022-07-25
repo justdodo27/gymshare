e9f9556c2ee91bb76a0509ba69c586e877013ff1
Mikołaj Simon <mikolajsimon.nt@gmail.com>
2022-07-25 19:54:57 +0200

    Create edit view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import HomePage from './components/homePage';
 import ChangePassword from './components/changePassword';
 import ResetPassword from './components/resetPassword';
 import Profile from './components/profileView';
+import EditProfile from './components/editProfile';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
     <Route path="/change" element={<ChangePassword/>} />
     <Route path="/forgot" element={<ResetPassword/>} />
     <Route path="/profile" element={<Profile/>} />
+    <Route path="/edit" element={<EditProfile/>} />
     </Routes>
   );
 }
