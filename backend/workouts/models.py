48c1a0883874c6a51e6edc34bc1e64c4bfba9473
Endrju00 <andrzej.kapczynski21@gmail.com>
2022-07-30 21:38:11 +0200

    Create models #13

---
+++ backend/workouts/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
+
+
+class Exercise(models.Model):
+    WITH_OWN_BODY_WEIGHT = 'With own body weight'
+    WITH_A_WEIGHT = 'With a weight'
+
+    EXERCISE_TYPES = [
+        (WITH_OWN_BODY_WEIGHT, 'With own body weight'),
+        (WITH_A_WEIGHT, 'With a weight'),
+    ]
+
+    title = models.CharField(max_length=80)
+    description = models.TextField(max_length=300, null=True, blank=True)
+    difficulty = models.PositiveIntegerField()
+    calories_burn_rate = models.FloatField(validators=[MinValueValidator(0)])
+    thumbnail = models.ImageField(null=True, blank=True)
+    video = models.FileField(null=True, blank=True)
+    exercise_type = models.CharField(max_length=30, choices=EXERCISE_TYPES, default=WITH_A_WEIGHT)
+
+
+class Workout(models.Model):
+    PUBLIC = 'Public'
+    HIDDEN = 'Hidden'
+
+    VISIBILITIES = [
+        (PUBLIC, 'Public'),
+        (HIDDEN, 'Hidden')
+    ]
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=80)
+    description = models.TextField(max_length=300, null=True, blank=True)
+    sum_of_cb = models.FloatField(null=True, blank=True)
+    difficulty = models.FloatField(null=True, blank=True)
+    visibility = models.CharField(max_length=20, choices=VISIBILITIES, default=PUBLIC)
+    avg_time = models.FloatField()
+    cycles = models.PositiveIntegerField()
+
+
+class ExcerciseInWorkout(models.Model):
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    order = models.PositiveIntegerField()
+    repeats = models.PositiveIntegerField(null=True, blank=True)
+    time = models.FloatField(null=True, blank=True)
+    series = models.PositiveIntegerField(null=True, blank=True)
