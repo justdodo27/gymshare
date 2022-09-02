80a1276122f0af35071e61ec60c559827e8ac8a4
Dominik Sawicki <dominik.sawicki52@gmail.com>
2022-09-03 00:09:50 +0200

    Add workouts searching, filtering and ordering

--- backend/workouts/views.py
+++ backend/workouts/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Q, When, Case
 
 from . import serializers, models
 
@@ -25,6 +27,10 @@ class WorkoutViewSet(viewsets.ModelViewSet):
     """
     queryset = models.Workout.objects.all()
     serializer_class = serializers.WorkoutSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'description', 'author__username']
+    filterset_fields = ['visibility',]
+    ordering_fields = ['title', 'sum_of_cb', 'difficulty', 'avg_time']
 
     def get_permissions(self):
         if self.action in ['create', 'update', 'partial_update', 'destroy']:
@@ -33,6 +39,14 @@ class WorkoutViewSet(viewsets.ModelViewSet):
             self.permission_classes = [permissions.AllowAny]
 
         return super().get_permissions()
+
+    def get_queryset(self):
+        queryset_for_hidden = (Q(visibility=models.Workout.HIDDEN) & Q(author=self.request.user))
+        queryset_for_public = Q(visibility=models.Workout.PUBLIC)
+
+        if self.request.user.is_anonymous:
+            return self.queryset.filter( queryset_for_public )    
+        return self.queryset.filter( queryset_for_public | queryset_for_hidden )
 
 
 class ExerciseInWorkoutViewSet(viewsets.ModelViewSet):
