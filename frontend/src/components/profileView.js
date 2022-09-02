47ff5293795150b6ee29ba555b6c69d6005c18f2
JustR4ndomUs3r <danielsz25@wp.pl>
2022-09-02 17:43:43 +0200

    ProfileWorkoutView

--- frontend/src/components/profileView.js
+++ frontend/src/components/profileView.js
@@ -18,6 +18,29 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Header from "../components/header.js"
 import { useState, useEffect } from "react";
 import { useSelector } from 'react-redux';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { styled } from '@mui/material/styles';
+import Collapse from '@mui/material/Collapse';
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 function Copyright(props) {
   return (
@@ -53,6 +76,7 @@ const sections = [
   { title: 'Logout', url: '/logout' },
 ];
 
+
 export default function Profile() {
 
   const userId = useSelector(state => state.auth.userId);
@@ -62,7 +86,15 @@ export default function Profile() {
   const [weight, setWeight] = useState(null)
   const [firstName, setFirstName] = useState(null)
   const [lastName, setLastName] = useState(null)
+  const [alignment, setAlignment] = React.useState('your');
+  const [expanded, setExpanded] = React.useState(false);
 
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+  const handleChange1 = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
 
   const fetchData = () => {
 
@@ -193,6 +225,113 @@ export default function Profile() {
               </Grid>
             </Grid>
         </Box>
+        <Box
+          sx={{
+            marginTop: 8,
+            marginBottom: 10,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            color: "primary.main",
+          }}
+        >
+
+    <ToggleButtonGroup
+      color="primary"
+      value={alignment}
+      exclusive
+      onChange={handleChange1}
+      aria-label="Platform"
+      size='large'
+    >
+      <ToggleButton size='medium' value="your">Your Workouts</ToggleButton>
+      <ToggleButton size='medium' value="liked">Liked Workouts</ToggleButton>
+    </ToggleButtonGroup>
+
+    {alignment==='your' && <Card sx={{ maxWidth: 545, minWidth: 645 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="340"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="gracjan r"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Trening Piwosza
+          </Typography>
+          <Typography gutterBottom variant="h7" component="div">
+            @dodo
+          </Typography>
+          <p>Rating: 2.1</p>
+          <p>Difficulty: 3.7</p>
+        </CardContent>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActionArea>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+        <Typography variant="body2" color="text.secondary">
+            Dupnij se browarka byq
+          </Typography>
+        </CardContent>
+        </Collapse>
+    </Card>}
+    {alignment==='liked' && <Card sx={{ maxWidth: 645, minWidth: 645 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="340"
+          image="/frontend/src/pictures/icon.jpg"
+          alt="mario pudzian"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Fiuto-pompki
+          </Typography>
+          <Typography gutterBottom variant="h7" component="div">
+            @PolskaGUROM200
+          </Typography>
+          <p>Rating: 2.1</p>
+          <p>Difficulty: 3.7</p>
+        </CardContent>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActionArea>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+        <Typography variant="body2" color="text.secondary">
+        Fiuto-pompki to ćwiczenie pobudzające zarówno mase mięśniową w okolicy bioder 
+            ale również brzucha. Idealne ćwiczenie dla początkującego ruchacza dymacza.
+          </Typography>
+        </CardContent>
+        </Collapse>
+    </Card>}
+    </Box>
         <Copyright sx={{ mt: 5 }} />
       </Container>
     </ThemeProvider>
