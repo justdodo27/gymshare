a821561050ffa4d728161c8d3d64f493ecb00adf
Endrju00 <75850182+Endrju00@users.noreply.github.com>
2022-10-23 20:24:32 +0200

    Fix #91

--- backend/workouts/utils.py
+++ backend/workouts/utils.py
@@ -1,11 +1,16 @@
+from django.shortcuts import get_object_or_404
+
 from accounts.models import Profile
 
 
 def get_user_weight(user):
     if not user.is_anonymous:
-        profile = Profile.objects.filter(user=user).first()
+        try:
+            profile = get_object_or_404(Profile, user=user)
+        except Profile.DoesNotExist:
+            return 65
 
-        if profile:
+        if profile.weight:
             return profile.weight
 
     return 65
