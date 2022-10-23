285d795ee1073701f069610e9a6dc7c6e589710a
Dominik Sawicki <dominik.sawicki52@gmail.com>
2022-10-23 22:12:50 +0200

    Add sort by id to workouts

--- backend/workouts/views.py
+++ backend/workouts/views.py
@@ -37,7 +37,7 @@ class WorkoutViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['title', 'description', 'author__username']
     filterset_fields = ['visibility', ]
-    ordering_fields = ['title', 'sum_of_cb', 'difficulty', 'avg_time']
+    ordering_fields = ['id', 'title', 'sum_of_cb', 'difficulty', 'avg_time']
     pagination_class = PageNumberPagination
     pagination_class.page_size = 15
 
