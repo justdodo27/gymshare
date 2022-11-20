db87b6a9d1d8a45b7f78639186f1a8124620e440
Dominik Sawicki <dominik.sawicki52@gmail.com>
2022-11-20 15:16:55 +0100

    Add order by date and filter by date's month & year

--- backend/stats/urls.py
+++ backend/stats/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('synchronize/', views.SyncStats.as_view()),
+    path('stats_calories/<int:month>/<int:year>/', views.StatisticCaloriesList.as_view()),
     path('stats_calories/', views.StatisticCaloriesList.as_view()),
     path('stats_calories/<int:pk>/', views.StatisticCaloriesDetail.as_view()),
     path('stats_exercise/', views.StatisticExerciseList.as_view()),
