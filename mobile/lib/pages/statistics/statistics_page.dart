6d1f869244e2c3f4c93a6fea72ac5a5ac4e26af3
Endrju00 <75850182+Endrju00@users.noreply.github.com>
2022-11-20 23:48:20 +0100

    Add date picker #107

--- mobile/lib/pages/statistics/statistics_page.dart
+++ mobile/lib/pages/statistics/statistics_page.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:gymshare/settings/colors.dart';
+import 'package:gymshare/components/widgets/date_picker_field.dart';
 
-class StatisticsPage extends StatelessWidget {
+class StatisticsPage extends StatefulWidget {
   const StatisticsPage({super.key});
 
   @override
+  State<StatisticsPage> createState() => _StatisticsPageState();
+}
+
+class _StatisticsPageState extends State<StatisticsPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.transparent,
       body: SafeArea(
-          child: Center(
-        child: Text(
-          'Statistics',
-          style: TextStyle(fontSize: 40, color: primaryTextColor),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                CustomDatePickerFormField(
+                  controller: _controller,
+                  onFieldSubmitted: (value) {
+                    final isValid = _formKey.currentState!.validate();
+                    if (isValid) {
+                      print('Valid');
+                    } else {
+                      print('Not valid');
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
-      )),
+      ),
     );
   }
 }
