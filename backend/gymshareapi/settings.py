160d3138b32ede150809ba559f5bb52badc9b2fa
Dominik Sawicki <dominik.sawicki52@gmail.com>
2022-12-29 20:38:47 +0100

    Configure django db for railway

--- backend/gymshareapi/settings.py
+++ backend/gymshareapi/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 from datetime import timedelta
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,8 +82,10 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD', 'aiograbddpxtkzyt')
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+DATABASE_URL = os.getenv("DATABASE_URL", None)
+
 DATABASES = {
-    'default': {
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800) if DATABASE_URL else {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('PGDATABASE'),
         'USER': os.environ.get('PGUSER'),
