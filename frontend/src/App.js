40a71a19ada67aff0a77f358e66a40ecf11e8654
Dominik Sawicki <dominik.sawicki52@gmail.com>
2023-01-22 14:01:17 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import ThemeProvider from './theme';
 import ScrollToTop from './components/ScrollToTop';
 import { BaseOptionChartStyle } from './components/chart/BaseOptionChart';
 
-// ----------------------------------------------------------------------
+//
 
 export default function App() {
   return (
