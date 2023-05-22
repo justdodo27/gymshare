4241aec619814ca47ece1e31bc1287f88bcf91a9
Dominik Sawicki <dominik.sawicki52@gmail.com>
2023-05-22 19:16:44 +0200

    change

--- backend/runbackend.sh
+++ backend/runbackend.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-<<<<<<< HEAD
 python manage.py migrate & python manage.py initstats & gunicorn gymshareapi.wsgi:application -w 100
-=======
-python manage.py migrate & python manage.py initstats & gunicorn gymshareapi.wsgi:application
->>>>>>> f05807a84c02fac1d8b93cced8805c7bbe4385b7
